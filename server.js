6531ad085922008afd316c092a0eda13af32c542
guilhermesalesdasilva768-cell <guilhermesalesdasilva768@gmail.com>
2025-09-17 06:12:35 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.post('/api/cadastrar', (req, res) => {
         if (row) return res.status(409).json({ status: 'erro', mensagem: 'Este e-mail já está cadastrado.' });
 
         const id = crypto.randomUUID();
-       const avatar = "https://via.placeholder.com/80";  
+       const avatar = `https://api.multiavatar.com/${encodeURIComponent(nome)}.svg`; 
 
         
         db.run(`INSERT INTO ranking (id, nome, email, senha, avatar, bip) VALUES (?, ?, ?, ?, ?, ?)`, 
